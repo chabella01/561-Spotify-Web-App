2d9eadc78c84654ff1247755bef9a17686f12a60
Jacob Flaxman <jacobflaxman1@gmail.com>
2023-09-11 10:26:28 -0700

    initial react create project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>561 Spotify Web App</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
